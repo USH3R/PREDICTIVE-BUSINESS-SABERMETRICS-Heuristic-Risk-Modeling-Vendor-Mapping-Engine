732002f332eb504b785c15bd9d9bb159b9adb0d1
Brian Dodd <104315834+USH3R@users.noreply.github.com>
2026-03-23 02:57:49 -0400

    Update risk_model.py

--- risk_model.py
+++ risk_model.py
@@ -2,14 +2,22 @@ import pandas as pd
 
 def calculate_risk(df):
 
-    sentiment_weight = 0.35
-    legacy_weight = 0.40
-    financial_weight = 0.25
+    sentiment_weight = 0.30
+    legacy_weight = 0.30
+    financial_weight = 0.20
+    fragility_weight = 0.20
+
+df["RiskScore"] = (
+    df["MarketSentiment"] * sentiment_weight +
+    df["LegacyDebt"] * legacy_weight +
+    (100 - df["FinancialVelocity"]) * financial_weight +
+    df["OperationalFragility"] * fragility_weight
 
     df["RiskScore"] = (
         df["MarketSentiment"] * sentiment_weight +
         df["LegacyDebt"] * legacy_weight +
-        (100 - df["FinancialVelocity"]) * financial_weight
+        (100 - df["FinancialVelocity"]) * financial_weight +
+        df["OperationalFragility"] * fragility_weight
     )
 
     return df
